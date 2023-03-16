739677f2448018f41fdadef573054a36e1e79dd3
jzhou-k <jiayizhou22918@gmail.com>
2023-03-16 19:55:05 -0400

    change structure

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import path from 'path';
+
 
 export default defineConfig({
     base: "/portfolio/", 
+    build: {
+        rollupOptions: {
+          input: {
+            main: path.resolve(__dirname, 'index.html'),
+            about: path.resolve(__dirname, 'projects/projects.html')
+          }
+        }
+      }
 })
+
